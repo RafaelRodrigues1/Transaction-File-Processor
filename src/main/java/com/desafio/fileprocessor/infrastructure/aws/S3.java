8bedf9bb6eb386ec03305d453d1348bfc4b2f8df
Rafael Rodrigues da Silva <rafaelx7t@hotmail.com>
2024-06-02 13:52:29 -0300

    s3 access and trasform .csv in dto

--- src/main/java/com/desafio/fileprocessor/infrastructure/aws/S3.java
+++ src/main/java/com/desafio/fileprocessor/infrastructure/aws/S3.java
@@ -1,29 +1,22 @@
 package com.desafio.fileprocessor.infrastructure.aws;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.ListBucketsRequest;
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 
-@Component
+@Configuration
 public class S3 {
 
     @Autowired
     private AwsConfig awsConfig;
 
-    public void build() {
-        S3Client s3 = S3Client.builder()
+    @Bean
+    public S3Client getS3Client() {
+        return S3Client.builder()
                 .credentialsProvider(awsConfig.credentialsProvider())
                 .region(awsConfig.getRegion())
                 .build();
-
-        ListBucketsRequest listBucketsRequest = ListBucketsRequest.builder().build();
-        ListBucketsResponse listBucketsResponse = s3.listBuckets(listBucketsRequest);
-
-        listBucketsResponse.buckets().forEach(bucket -> {
-            System.out.println(bucket.name());
-        });
     }
 }
